f8c2cc34be1b49a002de7b647596be9a1c0a33b5
jesheffield <whitetiger619@gmail.com>
2014-03-28 16:37:15 -0500

    -Fixed the prediction calculation

--- iPredict.py
+++ iPredict.py
@@ -3,8 +3,8 @@ from boto.mturk.question import QuestionContent,Question,QuestionForm, Overview,
 from time import sleep
 
 
-ACCESS_ID ='access id'
-SECRET_KEY = 'key'
+ACCESS_ID ='AKIAJRXDWKSWCOOVDQRA'
+SECRET_KEY = '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'
 HOST = 'mechanicalturk.sandbox.amazonaws.com'
  
 mtc = MTurkConnection(aws_access_key_id=ACCESS_ID,
@@ -33,7 +33,6 @@ ratings2 = [('Nothing','1'),
 		 
 yesNo = [('No', '0'),
 		('Yes','1')]
-		
 		
 tweet = 'Team A and Team B'
 
@@ -75,18 +74,18 @@ q1 = Question(identifier='nba',
 			  content=qc1,
 			  answer_spec=AnswerSpecification(fta1),
 			  is_required=True)
-			  
+
 #---------------  BUILD QUESTION 2 -------------------
  
 qc2 = QuestionContent()
-qc2.append_field('Title','...NFL?')
+qc2.append_field('Title','...NCAA? (March Madness)')
  
 fta2 = SelectionAnswer(min=1, max=1,style='radiobutton',
 					  selections=ratings,
 					  type='text',
 					  other=False)
  
-q2 = Question(identifier='nfl',
+q2 = Question(identifier='ncaa',
 			  content=qc2,
 			  answer_spec=AnswerSpecification(fta2),
 			  is_required=True)
@@ -95,7 +94,7 @@ q2 = Question(identifier='nfl',
 #---------------  BUILD QUESTION 3 -------------------
 
 qc3 = QuestionContent()
-#tweet = tweet + '\nAre there teams mentioned?'
+tweet = tweet + '\n\nIs a game mentioned?'
 qc3.append_field('Title', value= tweet)
  
 fta3 = SelectionAnswer(min=1, max=1,style='dropdown',
@@ -111,8 +110,7 @@ q3 = Question(identifier='teams',
  #---------------  BUILD QUESTION 4 -------------------
  
 qc4 = QuestionContent()
-tweet = tweet + '\n\n\nIf there are teams, enter first team. Otherwise, enter N/A.'
-qc4.append_field('Title',value = tweet)
+qc4.append_field('Title','If a game is mentioned, enter first team. Otherwise, enter N/A.')
  
 fta4 = FreeTextAnswer()
  
@@ -124,7 +122,7 @@ q4 = Question(identifier="team1",
  #---------------  BUILD QUESTION 5 -------------------
  
 qc5 = QuestionContent()
-qc5.append_field('Title','If there are teams, enter second team. Otherwise, enter N/A')
+qc5.append_field('Title','If a game is mentioned, enter second team. Otherwise, enter N/A')
  
 fta5 = FreeTextAnswer()
  
@@ -137,7 +135,7 @@ q5 = Question(identifier="team2",
 #---------------  BUILD QUESTION 6 -------------------
  
 qc6 = QuestionContent()
-qc6.append_field('Title','If there are teams, enter league they play in. Otherwise, enter N/A')
+qc6.append_field('Title','If is a game mentioned, enter league for game. Otherwise, enter N/A')
  
 fta6 = FreeTextAnswer()
  
@@ -152,13 +150,15 @@ question_form1 = QuestionForm()
 question_form1.append(overview)
 question_form1.append(q1)
 question_form1.append(q2)
-#question_form1.append(q3)
+question_form1.append(q3)
 question_form1.append(q4)
 question_form1.append(q5)
 question_form1.append(q6)
  
 #--------------- CREATE THE HIT -------------------
- 
+
+
+'''
 hit1 = mtc.create_hit(questions=question_form1,
 			   max_assignments=1,
 			   title=title,
@@ -168,6 +168,8 @@ hit1 = mtc.create_hit(questions=question_form1,
 			   reward=0.05)
 
 hit1_id=hit1[0].HITId
+
+
 
 #WAIT UNTIL ENOUGH RESPONSES HAVE BEEN GIVEN
 
@@ -181,7 +183,7 @@ while True:
 		break
 	
 print 'Done1'
-
+'''
 
 
 #Get data from hit1
@@ -190,7 +192,8 @@ teamsArray = ['A', 'B']
 timesTeamGiven=[3, 5]
 sportsArray = ['NBA']
 timesSportGiven = [5]
-
+tweetRelevant = False
+'''
 assignments1 = mtc.get_assignments(hit1_id)
 answerNum=0
 teamsIter = 0
@@ -201,35 +204,42 @@ for assignment in assignments1:
 		for value in question_form_answer.fields:
 			answerNum=answerNum+1
 			#print "Answer %s:" % answerNum
-			if answerNum == 3 or answerNum == 4:
-				teamsIter = 0
-				if value not in teamsArray:
-					teamsArray.append(value)
-					timesTeamGiven.append(1)
+			if answerNum == 3:
+				if value == 0:
+					tweetRelevant = False
 				else:
-					for y in teamsArray:
-						if y==value:
-							break
-						else:
-							teamsIter = teamsIter+1
-					#print 'Location is %s' % teamsIter
-					timesTeamGiven[teamsIter-1] = timesTeamGiven[teamsIter-1] + 1
-			elif answerNum == 5:
-				if value not in sportsArray:
-					sportsArray.append(value)
-					timesSportGiven.append(1)
-				else:
-					for y in sportsArray:
-						if y==value:
-							break
-						else:
-							sportsIter = sportsIter+1
-					#print 'Location is %s' % sportsIter
-					timesSportGiven[sportsIter-1] = timesSportGiven[sportsIter-1] + 1
+					tweetRelevant = True
+			if answerNum == 4 or answerNum == 5:
+				if tweetRelevant:
+					teamsIter = 0
+					if value not in teamsArray:
+						teamsArray.append(value)
+						timesTeamGiven.append(1)
+					else:
+						for y in teamsArray:
+							if y==value:
+								break
+							else:
+								teamsIter = teamsIter+1
+						#print 'Location is %s' % teamsIter
+						timesTeamGiven[teamsIter-1] = timesTeamGiven[teamsIter-1] + 1
+			if answerNum == 6:
+				if tweetRelevant:
+					if value not in sportsArray:
+						sportsArray.append(value)
+						timesSportGiven.append(1)
+					else:
+						for y in sportsArray:
+							if y==value:
+								break
+							else:
+								sportsIter = sportsIter+1
+						#print 'Location is %s' % sportsIter
+						timesSportGiven[sportsIter-1] = timesSportGiven[sportsIter-1] + 1
 			#print "%s" % value
 
 	#print "-------------------------"
-
+'''
 print teamsArray
 print timesTeamGiven
 print sportsArray
@@ -301,6 +311,23 @@ else:
 				content=qc8,
 				answer_spec=AnswerSpecification(fta8),
 				is_required=True)
+				
+	#---------------  BUILD QUESTION 4 -------------------
+	
+	qc9 = QuestionContent()
+	qc9.append_field('Title', 'Who do you want win?')
+	
+	teams2 = [(str(team1),'0'), (str(team2),'1'), ('Don\'t care', '2')]
+	
+	fta9 = SelectionAnswer(min=1,max=1,style='radiobutton',
+						selections=teams2,
+						type='text',
+						other=False)
+	
+	q9 = Question(identifier='prediction',
+				content=qc9,
+				answer_spec=AnswerSpecification(fta9),
+				is_required=True)
 	
 	#--------------- BUILD THE QUESTION FORM -------------------
 	
@@ -308,6 +335,7 @@ else:
 	question_form2.append(overview2)
 	question_form2.append(q7)
 	question_form2.append(q8)
+	question_form2.append(q9)
 	
 	#--------------- CREATE THE HIT -------------------
 	
@@ -352,11 +380,13 @@ else:
 				#print "Answer %s:" % answerNum
 				if answerNum == 1:
 					knowledgeValue = int(value)
-				elif answerNum == 2:
-					if value == 0:
-					    teamPredicts[0] = teamPredicts[0] + knowledgeValue
-					else:
+				if answerNum == 2:
+					if int(value) == int(0):
+						teamPredicts[0] = teamPredicts[0] + knowledgeValue
+					elif int(value) == int(1):
 						teamPredicts[1] = teamPredicts[1] + knowledgeValue
+					else:
+						print 'Nothing'
 				#print "%s" % value
 	
 		#print "-------------------------"
