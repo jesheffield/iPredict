59674089be406a49f896997c81c93e70dde64b0a
jesheffield <whitetiger619@gmail.com>
2014-03-28 13:21:15 -0500

    -Added functionality to monitor responses for first HIT (uses arrays to keep track of teams submitted,number of times each team was submitted, sports submitted,and number of times sport has been submitted)

--- iPredict.py
+++ iPredict.py
@@ -4,12 +4,12 @@ from time import sleep
 
 
 ACCESS_ID ='access id'
-SECRET_KEY = 'secret key'
+SECRET_KEY = 'key'
 HOST = 'mechanicalturk.sandbox.amazonaws.com'
  
 mtc = MTurkConnection(aws_access_key_id=ACCESS_ID,
-                      aws_secret_access_key=SECRET_KEY,
-                      host=HOST)
+					  aws_secret_access_key=SECRET_KEY,
+					  host=HOST)
  
 title = 'Sports questions'
 description = ('Answer questions about your knowledge of sports')
@@ -20,35 +20,35 @@ description2 = ('Answer questions and make a prediction')
 keywords2 = 'sports, prediction, question'
  
 ratings =[('Nothing','-2'),
-         ('A little','-1'),
-         ('Some','0'),
-         ('A lot','1'),
-         ('Everything','2')]
-         
+		 ('A little','-1'),
+		 ('Some','0'),
+		 ('A lot','1'),
+		 ('Everything','2')]
+		 
 yesNo = [('No', '0'),
-        ('Yes','1')]
-        
-        
+		('Yes','1')]
+		
+		
 tweet = 'Team A and Team B'
 
 def get_all_reviewable_hits(mtc):
-    page_size = 50
-    hits = mtc.get_reviewable_hits(page_size=page_size)
-    print "Total results to fetch %s " % hits.TotalNumResults
-    print "Request hits page %i" % 1
-    total_pages = float(hits.TotalNumResults)/page_size
-    int_total= int(total_pages)
-    if(total_pages-int_total>0):
-        total_pages = int_total+1
-    else:
-        total_pages = int_total
-    pn = 1
-    while pn < total_pages:
-        pn = pn + 1
-        print "Request hits page %i" % pn
-        temp_hits = mtc.get_reviewable_hits(page_size=page_size,page_number=pn)
-        hits.extend(temp_hits)
-    return hits
+	page_size = 50
+	hits = mtc.get_reviewable_hits(page_size=page_size)
+	print "Total results to fetch %s " % hits.TotalNumResults
+	print "Request hits page %i" % 1
+	total_pages = float(hits.TotalNumResults)/page_size
+	int_total= int(total_pages)
+	if(total_pages-int_total>0):
+		total_pages = int_total+1
+	else:
+		total_pages = int_total
+	pn = 1
+	while pn < total_pages:
+		pn = pn + 1
+		print "Request hits page %i" % pn
+		temp_hits = mtc.get_reviewable_hits(page_size=page_size,page_number=pn)
+		hits.extend(temp_hits)
+	return hits
  
 #---------------  BUILD OVERVIEW 1 -------------------
  
@@ -61,29 +61,29 @@ qc1 = QuestionContent()
 qc1.append_field('Title','...NBA?')
  
 fta1 = SelectionAnswer(min=1, max=1,style='radiobutton',
-                      selections=ratings,
-                      type='text',
-                      other=False)
+					  selections=ratings,
+					  type='text',
+					  other=False)
  
-q1 = Question(identifier='design',
-              content=qc1,
-              answer_spec=AnswerSpecification(fta1),
-              is_required=True)
-              
+q1 = Question(identifier='nba',
+			  content=qc1,
+			  answer_spec=AnswerSpecification(fta1),
+			  is_required=True)
+			  
 #---------------  BUILD QUESTION 2 -------------------
  
 qc2 = QuestionContent()
 qc2.append_field('Title','...NFL?')
  
 fta2 = SelectionAnswer(min=1, max=1,style='radiobutton',
-                      selections=ratings,
-                      type='text',
-                      other=False)
+					  selections=ratings,
+					  type='text',
+					  other=False)
  
-q2 = Question(identifier='design',
-              content=qc2,
-              answer_spec=AnswerSpecification(fta2),
-              is_required=True)
+q2 = Question(identifier='nfl',
+			  content=qc2,
+			  answer_spec=AnswerSpecification(fta2),
+			  is_required=True)
  
 #---------------  BUILD TWEET INFO -------------------
 #---------------  BUILD QUESTION 3 -------------------
@@ -93,14 +93,14 @@ tweet = tweet + '\nAre there teams mentioned?'
 qc3.append_field('Title', value= tweet)
  
 fta3 = SelectionAnswer(min=1, max=1,style='dropdown',
-                      selections=yesNo,
-                      type='text',
-                      other=False)
+					  selections=yesNo,
+					  type='text',
+					  other=False)
  
-q3 = Question(identifier='design',
-              content=qc3,
-              answer_spec=AnswerSpecification(fta3),
-              is_required=True)
+q3 = Question(identifier='teams',
+			  content=qc3,
+			  answer_spec=AnswerSpecification(fta3),
+			  is_required=True)
  
  #---------------  BUILD QUESTION 4 -------------------
  
@@ -110,9 +110,9 @@ qc4.append_field('Title','If there are teams, enter first team. Otherwise, enter
 fta4 = FreeTextAnswer()
  
 q4 = Question(identifier="team1",
-              content=qc4,
-              answer_spec=AnswerSpecification(fta4),
-              is_required=True)
+			  content=qc4,
+			  answer_spec=AnswerSpecification(fta4),
+			  is_required=True)
 
  #---------------  BUILD QUESTION 5 -------------------
  
@@ -122,9 +122,9 @@ qc5.append_field('Title','If there are teams, enter second team. Otherwise, ente
 fta5 = FreeTextAnswer()
  
 q5 = Question(identifier="team2",
-              content=qc5,
-              answer_spec=AnswerSpecification(fta5),
-              is_required=True)
+			  content=qc5,
+			  answer_spec=AnswerSpecification(fta5),
+			  is_required=True)
 			  
 			  
 #---------------  BUILD QUESTION 6 -------------------
@@ -135,9 +135,9 @@ qc6.append_field('Title','If there are teams, enter sport they play. Otherwise, 
 fta6 = FreeTextAnswer()
  
 q6 = Question(identifier="sport",
-              content=qc6,
-              answer_spec=AnswerSpecification(fta6),
-              is_required=True)
+			  content=qc6,
+			  answer_spec=AnswerSpecification(fta6),
+			  is_required=True)
  
 #--------------- BUILD THE QUESTION FORM -------------------
  
@@ -153,27 +153,76 @@ question_form1.append(q6)
 #--------------- CREATE THE HIT -------------------
  
 hit1 = mtc.create_hit(questions=question_form1,
-               max_assignments=1,
-               title=title,
-               description=description,
-               keywords=keywords,
-               duration = 60*5,
-               reward=0.05)
+			   max_assignments=1,
+			   title=title,
+			   description=description,
+			   keywords=keywords,
+			   duration = 60*5,
+			   reward=0.05)
 
 hit1_id=hit1[0].HITId
 
 #WAIT UNTIL ENOUGH RESPONSES HAVE BEEN GIVEN
 
 while True:
-    assignments1 = mtc.get_assignments(hit1_id)
-    num1 = int(assignments1.NumResults)
-    if num1 < 1:
-        print 'Not done1'
-        continue
-    else:
-        break
-    
+	assignments1 = mtc.get_assignments(hit1_id)
+	num1 = int(assignments1.NumResults)
+	if num1 < 1:
+		#print 'Not done1'
+		continue
+	else:
+		break
+	
 print 'Done1'
+
+teamsArray = ['A', 'B']
+timesTeamGiven=[3, 5]
+sportsArray = ['NBA']
+timesSportGiven = [5]
+#Get data from hit1
+assignments1 = mtc.get_assignments(hit1_id)
+answerNum=0
+teamsIter = 0
+sportsIter = 0
+for assignment in assignments1:
+	print "Answers of the worker %s" % assignment.WorkerId
+	for question_form_answer in assignment.answers[0]:
+		for value in question_form_answer.fields:
+			answerNum=answerNum+1
+			print "Answer %s:" % answerNum
+			if answerNum == 4 or answerNum == 5:
+				teamsIter = 0
+				if value not in teamsArray:
+					teamsArray.append(value)
+					timesTeamGiven.append(1)
+				else:
+					for y in teamsArray:
+						if y==value:
+							break
+						else:
+							teamsIter = teamsIter+1
+					print 'Location is %s' % teamsIter
+					timesTeamGiven[teamsIter-1] = timesTeamGiven[teamsIter-1] + 1
+			elif answerNum == 6:
+				if value not in sportsArray:
+					sportsArray.append(value)
+					timesSportGiven.append(1)
+				else:
+					for y in enumerate(sportsArray):
+						if y==value:
+							break
+						else:
+							sportsIter = sportsIter+1
+					print 'Location is %s' % sportsIter
+					timesSportGiven[sportsIter-1] = timesSportGiven[sportsIter-1] + 1
+			print "%s" % value
+
+	print "-------------------------"
+
+print teamsArray
+print timesTeamGiven
+print sportsArray
+print timesSportGiven
 
 
 #---------------  BUILD OVERVIEW 2 -------------------
@@ -189,14 +238,14 @@ question = '...' + sport + '?'
 qc7.append_field('Title',value=question)
  
 fta7 = SelectionAnswer(min=1, max=1,style='radiobutton',
-                      selections=ratings,
-                      type='text',
-                      other=False)
+					  selections=ratings,
+					  type='text',
+					  other=False)
  
-q7 = Question(identifier='design',
-              content=qc7,
-              answer_spec=AnswerSpecification(fta7),
-              is_required=True)
+q7 = Question(identifier='nba',
+			  content=qc7,
+			  answer_spec=AnswerSpecification(fta7),
+			  is_required=True)
  
 #---------------  BUILD TWEET INFO -------------------
 #---------------  BUILD QUESTION 3 -------------------
@@ -209,14 +258,14 @@ predictionTeams = team1 + ' vs ' + team2 + '\n\nWho will win?'
 qc8.append_field('Title', value= predictionTeams)
  
 fta8 = SelectionAnswer(min=1,max=1,style='radiobutton',
-                      selections=teams,
-                      type='text',
-                      other=False)
+					  selections=teams,
+					  type='text',
+					  other=False)
  
-q8 = Question(identifier='design',
-              content=qc8,
-              answer_spec=AnswerSpecification(fta8),
-              is_required=True)
+q8 = Question(identifier='prediction',
+			  content=qc8,
+			  answer_spec=AnswerSpecification(fta8),
+			  is_required=True)
  
 #--------------- BUILD THE QUESTION FORM -------------------
  
@@ -226,28 +275,29 @@ question_form2.append(q7)
 question_form2.append(q8)
  
 #--------------- CREATE THE HIT -------------------
- 
+'''
 hit2 = mtc.create_hit(questions=question_form2,
-               max_assignments=1,
-               title=title2,
-               description=description2,
-               keywords=keywords2,
-               duration = 60*5,
-               reward=0.05)
+			   max_assignments=1,
+			   title=title2,
+			   description=description2,
+			   keywords=keywords2,
+			   duration = 60*5,
+			   reward=0.05)
 
 hit2_id=hit2[0].HITId
 
 #WAIT UNTIL ENOUGH RESPONSES HAVE BEEN GIVEN
 
 while True:
-    assignments2 = mtc.get_assignments(hit2_id)
-    num2 = int(assignments2.NumResults)
-    if num2 < 1:
-        print 'Not done2'
-        continue
-    else:
-        break
-    
+	assignments2 = mtc.get_assignments(hit2_id)
+	num2 = int(assignments2.NumResults)
+	if num2 < 1:
+		print 'Not done2'
+		continue
+	else:
+		break
+'''
 print 'Done2'
 
 
+#Get data from part 2
